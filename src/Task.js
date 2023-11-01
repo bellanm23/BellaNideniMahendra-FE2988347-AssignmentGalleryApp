fdfa0554f37fdcb1b671988296a8d57889ebdddc
bellanm23 <bella.nideni@students.utdi.ac.id>
2023-11-01 13:34:01 +0700

    Update Assignment Deployment Netlify

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://bellanidenimahendra-fe2988347.netlify.app/" // TODO: replace this
 const studentName = "BELLA NIDENI MAHENDRA" // TODO: replace this
 const studentId = "FE2988347" // TODO: replace this
 
